24e7a700f236daac07e4721ead3e5c89137ea035
Amy Tang <amy@pathfactory.com>
2019-10-29 11:09:43 -0400

    initial commit

--- api/routes/jobs/jobRoutes.js
+++ api/routes/jobs/jobRoutes.js
@@ -1,1 +1,28 @@
+const express = require("express");
+const router = express.Router();
+
 const jobs = [];
+
+router.route("/").get((req, res) => {
+  // 1. Respond with the array of jobs
+  res.status(200).send({
+    data: jobs
+  });
+});
+
+router.route("/add").post((req, res) => {
+  // 1. Grab the new job infomration from the request body
+  const { title, company, link } = req.body;
+
+  // 2. Push the job to our job array
+  jobs.push({
+    title: title,
+    company: company,
+    link: link
+  });
+
+  // 3. Respond with the updated jobs array
+  res.status(200).send({
+    data: jobs
+  });
+});
